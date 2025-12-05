b825887fda4bb2c2ef087b6e93fad7c12d0feea5
Syahril Sulaiman <syahrilrevolusi@gmail.com>
2025-12-05 23:38:01 +0800

    teng commit - increase file upload size

--- configs/fileUpload.js
+++ configs/fileUpload.js
@@ -83,7 +83,7 @@ const upload = multer({
     storage: storage,
     fileFilter: fileFilter,
     limits: {
-        fileSize: 10 * 1024 * 1024 // 10MB limit
+        fileSize: 15 * 1024 * 1024 // 10MB limit
     }
 })
 
