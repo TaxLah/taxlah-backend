0dbacebd60ac61fcbe61cb4af61207769ffcfcbc
Syahril Sulaiman <syahrilrevolusi@gmail.com>
2025-12-05 14:51:01 +0800

    teng commit - betulkan function crypto

--- utils/crypto.js
+++ utils/crypto.js
@@ -1,16 +1,16 @@
-import crypto from "crypto"
+const crypto = require("crypto")
 
 const key   = Buffer.from("12345678901234567890123456789012"); // same as frontend
 const iv    = Buffer.from("1234567890123456");
 
-export function encryptData(data) {
+function encryptData(data) {
     const cipher    = crypto.createCipheriv("aes-256-cbc", key, iv);
     let encrypted   = cipher.update(JSON.stringify(data), "utf8", "base64");
     encrypted       += cipher.final("base64");
     return encrypted;
 }
 
-export function decryptData(encrypted) {
+function decryptData(encrypted) {
     const decipher    = crypto.createDecipheriv("aes-256-cbc", key, iv);
     let decrypted     = decipher.update(encrypted, "base64", "utf8");
     decrypted         += decipher.final("utf8");
@@ -19,7 +19,7 @@ export function decryptData(encrypted) {
 }
 
 // Middleware
-export function decryptMiddleware(req, res, next) {
+function decryptMiddleware(req, res, next) {
   try {
         if (!req.body.data) return res.status(400).json({ error: "Missing encrypted data" });
         const decrypted     = decryptData(req.body.data);
@@ -30,4 +30,10 @@ export function decryptMiddleware(req, res, next) {
         console.error("Decryption failed:", err.message);
         return res.status(400).json({ error: "Invalid encrypted payload" });
   }
+}
+
+module.exports = {
+    encryptData,
+    decryptData,
+    decryptMiddleware
 }
