6cc7002dda26b9dca2089d0902b9594a40a53efb
Syahril Sulaiman <syahrilrevolusi@gmail.com>
2026-05-05 22:16:50 +0800

    FIxed: Update cors

--- configs/cors.js
+++ configs/cors.js
@@ -1,13 +1,21 @@
-var whitelist = ['https://dev.taxlah.com', 'https://staging.taxlah.com', 'https://taxlah.com', 'https://localhost:3000', 'https://sysadmin.taxlah.com']
+var whitelist = [
+	"https://dev.taxlah.com",
+	"https://staging.taxlah.com",
+	"https://taxlah.com",
+	"https://localhost:3000",
+	"https://localhost:4000",
+	"https://localhost:5000",
+	"https://sysadmin.taxlah.com",
+];
 var corsOptions = {
-  origin: function (origin, callback) {
-    console.log("Log Origin : ", origin)
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
+	origin: function (origin, callback) {
+		console.log("Log Origin : ", origin);
+		if (whitelist.indexOf(origin) !== -1) {
+			callback(null, true);
+		} else {
+			callback(new Error("Not allowed by CORS"));
+		}
+	},
+};
 
-module.exports = corsOptions
+module.exports = corsOptions;
